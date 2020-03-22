c7958fab3120d65e36072ccbaf852beaeae47579
Tuscany <lizlevine.rigie@gmail.com>
2020-03-22 15:17:21 -0400

    feat: Add images and links, including imageDetails file

---
+++ click-reward/src/components/ImgCard/imageDetails.js
@@ -0,0 +1,76 @@
+export default {
+  allImages: [
+    {
+      id: "0",
+      altTxt: "Beagle",
+      image: "./image/gracie.jpg"
+      // image: 'https://c402277.ssl.cf1.rackcdn.com/photos/1257/images/hero_small/SeaTurtle-1600x600px.jpg?1345590072'
+    },
+    {
+      id: "1",
+      altTxt: "Coon Hound",
+      image: "images/coonHound.jpg"
+      // image: 'https://focusingonwildlife.com/news/wp-content/uploads/grizzly-bear-family-wallpaper.jpg'
+    },
+    {
+      id: "2",
+      altTxt: "Cardigan Welsh Corgie",
+      image: "./images/corgy.jpg"
+      // image: 'http://4.bp.blogspot.com/-K9IABSDmxu4/VfzqUs9gVjI/AAAAAAAAA4I/UZeMPjpSe6w/s1600/endangered%2Bcheetah.jpg'
+    },
+    {
+      id: "3",
+      altTxt: "Rhodesian Ridgeback",
+      image: "./images/ridgeback.jpg"
+      // image: 'https://d3i6fh83elv35t.cloudfront.net/static/2017/08/baldeagle-1024x645.jpg'
+    },
+    {
+      id: "4",
+      altTxt: "Shiba Enu",
+      image: "./image/shiba.jpg"
+      // image: 'https://assets.nrdc.org/sites/default/files/styles/full_content--retina/public/media-uploads/wlds43_654640_2400.jpg?itok=LbhnLIk9'
+    },
+    {
+      id: "5",
+      altTxt: "dog with thought bubble",
+      image: "./images/silly.jpg"
+      // image: 'https://cdn.vox-cdn.com/thumbor/Ndn73qyjkLmb4ocWrRg2YPFQVz4=/0x0:6000x4000/1200x800/filters:focal(4437x534:5397x1494)/cdn.vox-cdn.com/uploads/chorus_image/image/62369619/26003376186_7833972fd3_o.0.jpg'
+    },
+    {
+      id: "6",
+      altTxt: "Beagle",
+      image: "./images/gracie_2.jpg"
+      // image: 'https://media.greenmatters.com/brand-img/06ZmYhDZp/0x0/masai-giraffe-extinction-1563306007202.jpg'
+    },
+    {
+      id: "7",
+      altTxt: "talking dog",
+      image: "./images/clickTreat.jpg"
+      // image: 'http://advocacy.britannica.com/blog/advocacy/wp-content/uploads/132325-131-40560F7E.jpg'
+    },
+    {
+      id: "8",
+      altTxt: "Shepherd pup",
+      image: "./images/major.jpg"
+      // image: 'https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/ocelot.bmp?crop=0,88,1024,563&wid=4000&hei=2200&scl=0.256'
+    },
+    {
+      id: "9",
+      altTxt: "Basenji",
+      image: "./images/basenji.jpg"
+      // image: 'https://static.scientificamerican.com/blogs/cache/file/65367319-B08B-4C77-8A2F42A5E05C8B53_source.jpg?w=590&h=800&D6D8E7BF-D19C-4E8C-90079A9237B3FE79'
+    },
+    {
+      id: "10",
+      altTxt: "Bernese Mountain pup",
+      image: "./images/zoe.jpg"
+      // image: 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492109387/articles/2016/09/07/what-happens-when-animals-get-off-the-endangered-list/160907-slattery-animals-off-endangered-list-tease_jlrsul'
+    },
+    {
+      id: "11",
+      altTxt: "Aussie Shepherd Mix",
+      image: "./images/maisy.jpg"
+      // image: 'https://animalogic.ca/wp-content/uploads/2018/12/1024px-Eisba%CC%88r_1996-07-23.jpg'
+    }
+  ]
+};
