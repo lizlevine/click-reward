f43c3341dfe50b9e7971ff6a8535fb33e516b291
Tuscany <lizlevine.rigie@gmail.com>
2020-03-23 15:09:25 -0400

    docs: Fix folder structure, fix babel package

---
+++ build/precache-manifest.f6ac93a9bf8fc51240ef1dcc866e30a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261d2c4092bd7ba92fc333cdcc214922",
+    "url": "/click-reward/index.html"
+  },
+  {
+    "revision": "7824b79713c00678419e",
+    "url": "/click-reward/static/css/main.5dae0b21.chunk.css"
+  },
+  {
+    "revision": "f9ee0b5e71839b826961",
+    "url": "/click-reward/static/js/2.910c2fd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-reward/static/js/2.910c2fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7824b79713c00678419e",
+    "url": "/click-reward/static/js/main.738a0a72.chunk.js"
+  },
+  {
+    "revision": "e378ebc705f06235078c",
+    "url": "/click-reward/static/js/runtime-main.e8a6695d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-reward/static/media/logo.5d5d9eef.svg"
+  }
+]);
